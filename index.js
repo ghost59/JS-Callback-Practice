5cdccd36a0a028e55a7e4b77a15cde5dd4392b96
ghost59 <as.1714554@gmail.com>
2023-05-22 21:30:57 -0400

    edits

--- index.js
+++ index.js
@@ -2,10 +2,60 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+move(character).withArrowKeys(100, 250)
+let direction = null; 
+let x = 100; 
+let y = 250; 
 
 
-move(newImage('assets/tree.png')).to(200, 450)
+function handleDirectionChange() {
+  if (direction === null) {
+    character.src = 'assets/green-character/static.gif'
+  }
+  if (direction === 'west') {
+    character.src = 'assets/green-character/west.gif'
+  }
+  if (direction === 'north') {
+    character.src = 'assets/green-character/north.gif'
+  }
+  if (direction === 'east') {
+    character.src = 'assets/green-character/east.gif'
+  }
+  if (direction === 'south') {
+    character.src = 'assets/green-character/south.gif'
+  }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+
+setInterval(moveCharacter,1); 
+document.addEventListener('keydown',function(e){
+
+  if(e.reapet) return; 
+
+
+  if (e.key === 'ArrowLeft') {
+    direction = 'west'
+  }
+  if(e.key === 'ArrowUp') {
+    direction = 'north'
+  }
+  if(e.key === 'ArrowRight') {
+    direction = 'east'
+  }
+  if(e.key === 'ArrowDown') {
+    direction = 'south'
+  }
+  
+
+})
+document.addEventListener('keyup', function (e) {
+  direction = null
+})
+ 
+
+move(newImage('assets/tree.png')).withArrowKeys(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
